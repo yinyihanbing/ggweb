2b529d5f10e8b080dbf4440a5cdad7df2c5e7ae2
李潘彬 <yinyihanbing@gmail.com>
2025-02-28 16:11:01 +0800

    no message

--- src/stores/roleStore.ts
+++ src/stores/roleStore.ts
@@ -3,9 +3,9 @@ import { defineStore } from 'pinia'
 
 export const useRoleStore = defineStore('role', () => {
   const roles = ref([
-    { id: 1, name: 'Admin', status: 'a1' },
-    { id: 2, name: 'User', status: 'a2' },
-    { id: 3, name: 'Guest', status: 'a6' }
+    { id: 1, name: 'Admin2', status: 'a1' },
+    { id: 2, name: 'User2', status: 'a2' },
+    { id: 3, name: 'Guest2', status: 'a6' }
   ])
 
   return { roles }
